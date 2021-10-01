3fe10eed4587f5fc66d99ce7d1f544b524ddd69f
priyesh-roy <71977590+priyesh-roy@users.noreply.github.com>
2021-10-01 09:20:24 +0530

    Update 63. Floyd's Triangle.c

--- 63. Floyd's Triangle.c
+++ 63. Floyd's Triangle.c
@@ -2,10 +2,10 @@
 int main() {
    int rows, i, j, number = 1;
    printf("Enter the number of rows: ");
-   scanf("&d", &rows);
+   scanf("%d", &rows);
    for (i = 1; i <= rows; i++) {
       for (j = 1; j <= i; ++j) {
-         printf("&d ", number);
+         printf("%d ", number);
          ++number;
       }
       printf("\n");
